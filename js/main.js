a4994ad04f656acda9d6cf4242716f1a3568161f
Yves Debongnie <yves.debongnie@gmail.com>
2018-09-16 00:07:26 +0200

    Styling mobile menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
  jQuery(document).ready( function () {
-     jQuery('a[href^="#"]').click( function (e) {
+     jQuery('a[href^="#"]:not([href="#"]').click( function (e) {
          e.preventDefault();
          var target = jQuery(e.currentTarget.hash);
-         jQuery("html, body").animate( { scrollTop: target.offset().top }, 400);
+         jQuery('.menu-wrapper').animate({'left':'-100vw'}, 200);
+         jQuery("html, body").animate( { scrollTop: target.offset().top }, 700);
      });
+    
+    jQuery('.open-menu-button').click(function (e) {
+        e.preventDefault();
+        jQuery('.menu-wrapper').animate({'left':'0'});
+    });
+    
+    jQuery('.close-menu-button').click(function (e) {
+        e.preventDefault();
+        jQuery('.menu-wrapper').animate({'left':'-100vw'});
+    });
+        
  });
+ 
