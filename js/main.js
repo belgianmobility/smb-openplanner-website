f49df50fcc46efd5fe75b0d28e268c4e33bd192c
Mathieu Delvaux <mathieudelvaux.567@hotmail.com>
2018-09-17 08:21:00 +0200

    fix(Global): design review + JS fix & improvement + basic motion design

--- js/main.js
+++ js/main.js
@@ -1,20 +1,75 @@
- jQuery(document).ready( function () {
-     jQuery('a[href^="#"]:not([href="#"]').click( function (e) {
-         e.preventDefault();
-         var target = jQuery(e.currentTarget.hash);
-         jQuery('.menu-wrapper').animate({'left':'-100vw'}, 200);
-         jQuery("html, body").animate( { scrollTop: target.offset().top }, 700);
-     });
-    
-    jQuery('.open-menu-button').click(function (e) {
-        e.preventDefault();
-        jQuery('.menu-wrapper').animate({'left':'0'});
-    });
-    
-    jQuery('.close-menu-button').click(function (e) {
-        e.preventDefault();
-        jQuery('.menu-wrapper').animate({'left':'-100vw'});
-    });
-        
- });
- 
+jQuery(document).ready( function () {
+  jQuery('a[href*="#"]').
+    not('[href="#"]').
+    not('[href="#0"]').
+    click(function(event) {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        var target = jQuery(this.hash);
+        target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          if (jQuery('.menu-wrapper').hasClass('nav-open')) {
+            jQuery('.menu-wrapper').removeClass('nav-open');
+          }
+          scrollTo(target.offset().top, focus(target), 1600);
+        }
+      }
+  });
+
+  jQuery('.open-menu-button').click(function (e) {
+    jQuery('.menu-wrapper').addClass('nav-open');
+  });
+
+  jQuery('.close-menu-button').click(function (e) {
+    jQuery('.menu-wrapper').removeClass('nav-open');
+  });
+});
+
+
+function focus(el) {
+  el.focus();
+  if (el.is(":focus")) {
+    return false;
+  } else {
+    el.attr('tabindex','-1');
+    el.focus();
+  };
+}
+
+Math.easeInOutExpo = function (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2 * Math.pow( 2, 10 * (t - 1) ) + b;
+	t--;
+	return c/2 * ( -Math.pow( 2, -10 * t) + 2 ) + b;
+};
+
+function scrollTo(to, callback, duration) {
+  function move(amount) {
+    document.scrollingElement.scrollTop = amount;
+  }
+  function position() {
+    return document.scrollingElement.scrollTop;
+  }
+  var start = position(),
+    change = to - start,
+    currentTime = 0,
+    increment = 20;
+  duration = (typeof(duration) === 'undefined') ? 800 : duration;
+  var animateScroll = function() {
+    currentTime += increment;
+    var val = Math.easeInOutExpo(currentTime, start, change, duration);
+    move(val);
+    if (currentTime < duration) {
+      window.requestAnimationFrame(animateScroll);
+    } else {
+      if (callback && typeof(callback) === 'function') {
+        callback();
+      }
+    }
+  };
+  animateScroll();
+}
